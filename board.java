d1c969074591617b8f8293d49258aa21f9b34944
ralgier <36775917+ralgier@users.noreply.github.com>
2018-10-02 16:24:20 -0400

    Add files via upload

--- board.java
+++ board.java
@@ -13,73 +13,111 @@ public class board {
 		String restart = new String();
 		String quit = new String();
 
-			for (int row = 0; row < theBoard.length; row++) {
-				for (int col = 0; col < theBoard[row].length; col++) {
+		for (int row = 0; row < theBoard.length; row++) {
+			for (int col = 0; col < theBoard[row].length; col++) {
 
-					theBoard[row][col] = 0;
+				theBoard[row][col] = 0;
 
-				}
 			}
-			int x = newNum.nextInt(100) + 1;
-			int y = newNum.nextInt(100) + 1;
-			int randRow = newNum.nextInt(4);
-			int randCol = newNum.nextInt(4);
-			int randRow2 = newNum.nextInt(4);
-			int randCol2 = newNum.nextInt(4);
-			if(theBoard[randRow] == theBoard[randRow2] && theBoard[randCol] == theBoard[randCol2]) {
-					randRow2 = newNum.nextInt(4);
-					randCol2 = newNum.nextInt(4);
-			}
-			if (x < 80) {
-				theBoard[randRow][randCol] = 2;
+		}
+		int x = newNum.nextInt(100) + 1;
+		int y = newNum.nextInt(100) + 1;
+		int randRow = newNum.nextInt(4);
+		int randCol = newNum.nextInt(4);
+		int randRow2 = newNum.nextInt(4);
+		int randCol2 = newNum.nextInt(4);
+
+		if (theBoard[randRow] == theBoard[randRow2] && theBoard[randCol] == theBoard[randCol2]) {
+			randRow2 = newNum.nextInt(4);
+			randCol2 = newNum.nextInt(4);
+		}
+
+		if (x < 80) {
+			theBoard[randRow][randCol] = 2;
+		} else {
+			theBoard[randRow][randCol] = 4;
+		}
+
+		if (counter == 0) {
+			if (y < 80) {
+				theBoard[randRow2][randCol2] = 2;
 			} else {
-				theBoard[randRow][randCol] = 4;
+				theBoard[randRow2][randCol2] = 4;
 			}
+		}
 
-			if (counter == 0) {
-				if (y < 80) {
-					theBoard[randRow2][randCol2] = 2;
-				} else {
-					theBoard[randRow2][randCol2] = 4;
+		for (int row = 0; row < theBoard.length; row++) {
+			for (int col = 0; col < theBoard[row].length; col++) {
+				System.out.print(theBoard[row][col] + " ");
+			}
+			System.out.println(" ");
+		}
+//STARTING HERE: get input from the user for movement direction
+		char typed;
+		do {
+			System.out.println("w = up; a = left; s = down; d = right; q = quit; r = restart: ");
+
+			typed = scan.next().charAt(0);
+
+			if (typed == 'w') {
+				moveUp();
+
+			} else if (typed == 'a') {
+				moveRight();
+
+			} else if (typed == 's') {
+				moveDown();
+
+			} else if (typed == 'd') {
+				moveRight();
+
+			} else if (typed == 'r') {
+
+				System.out.print("Are you sure? Enter r again to restart: ");
+				typed = scan.next().charAt(0);
+				if (typed == 'r') {
+					restart();
 				}
-			}
 
-			for (int row = 0; row < theBoard.length; row++) {
-				for (int col = 0; col < theBoard[row].length; col++) {
-					System.out.print(theBoard[row][col] + " ");
+			} else if (typed == 'q') {
 
+				System.out.print("Are you sure? Enter q again to quit or w to keep playing: ");
+				typed = scan.next().charAt(0);
+
+				if (typed == 'q') {
+					quit();
 				}
-				System.out.println(" ");
+
+			} else {
+
+				System.out.println("That is not a valid input. Please enter w,a,s,d,r or q");
 			}
-			
-			
-			newRandomNumber(theBoard);
-			
-			
+		} while (typed != 'q' && typed != 'r');
+
 	}
-	
-	public static void newRandomNumber(int [][] board) {
+
+	public static void newRandomNumber(int[][] board) {
 		Random newNum = null;
 		int a = newNum.nextInt(100) + 1;
 		int b = newNum.nextInt(100) + 1;
 		int randRow = newNum.nextInt(4);
 		int randCol = newNum.nextInt(4);
-		
-		
-		//we need to put this in some sort of loop to check every value of the array 
-		
+
+		// we need to put this in some sort of loop to check every value of the array
+
 		if (a < 80) {
-			//we need to test if the randomly selected location is empty or not, else we need to run again
+			// we need to test if the randomly selected location is empty or not, else we
+			// need to run again
 			if (board[randRow][randCol] == 0) {
 				board[randRow][randCol] = 2;
-			} 
+			}
 		} else {
 			if (board[randRow][randCol] == 0) {
 				board[randRow][randCol] = 4;
 			}
 		}
-		
-		//print out the new board again 
+
+		// print out the new board again
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
 				System.out.print(board[row][col] + " ");
@@ -87,11 +125,12 @@ public class board {
 			}
 			System.out.println(" ");
 		}
-		
+
 	}
+
 	public static void moveUp() {
 		System.out.println("up");
-	//	board.newRandomNumber(theBoard);
+		// board.newRandomNumber(theBoard);
 	}
 
 	public static void moveLeft() {
@@ -104,15 +143,15 @@ public class board {
 
 	public static void moveRight() {
 		System.out.println("right");
-		
-//		for (int q = 0; q < myArray.length; q++) {
-//			for (int w = 0; w < myArray[0].length - 1; w++) {
-//				myArray[q][w + 1] = myArray[q][w + 1] + myArray[q][w];
-//
-//			}
-//
-//		}
-//		lab3.print2dArray(myArray);
+
+		// for (int q = 0; q < myArray.length; q++) {
+		// for (int w = 0; w < myArray[0].length - 1; w++) {
+		// myArray[q][w + 1] = myArray[q][w + 1] + myArray[q][w];
+		//
+		// }
+		//
+		// }
+		// lab3.print2dArray(myArray);
 	}
 
 	public static void restart() {
@@ -122,8 +161,7 @@ public class board {
 
 	public static void quit() {
 
-		//System.out.println("game over. Moves made: " + counter);
+		// System.out.println("game over. Moves made: " + counter);
 	}
-	
-	
+
 }
