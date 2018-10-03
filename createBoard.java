aa127cfdd1b8f1b55c3cbcd2213b1dbfacabbf6d
ralgier <36775917+ralgier@users.noreply.github.com>
2018-10-03 00:47:46 -0400

    Update createBoard.java

--- createBoard.java
+++ createBoard.java
@@ -1,6 +1,5 @@
-
 public class createBoard {
-	//method to test if the board is full, returns value of boolean
+	// method to test if the board is full, returns value of boolean
 	public static boolean boardFull(int[][] theBoard) {
 
 		int rows = theBoard.length;
@@ -28,27 +27,92 @@ public class createBoard {
 			}
 		}
 		return boardFull;
+	}
+
+	// create method to print the current maximum of the array (needs to be called
+	// every time)
+	public static void printMax(int[][] board) {
+		int max = 2;
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+
+				if (board[row][col] > max) {
+					max = board[row][max];
+				}
+
+			}
+		}
+		System.out.println(max);
 
 	}
-	
-	public static boolean Test() {
-		
-		int x = 5;
-		int y = 5;
-		
-		boolean Test = false;
-		
-		if (x == y) {
-			Test = true;
+
+	// method to test if you can make a move to the right
+	//
+	public static boolean testRight(int[][] board) {
+		boolean testRight = false;
+		// test if there is a location where there are two spaces in the same row next
+		// to each other that are equal
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length - 1; j++) {
+				if (board[i][j] == board[i][j + 1]) {
+					testRight = true;
+				}
+			}
 		}
+		// test if there are any equal values in the same row 2 apart with a 0 in the middle
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length - 2; j++) {
+				if (board[i][j] == board[i][j + 2] && board[i][j + 1] == 0) {
+					testRight = true;
+				} 
+			}
+		}
+		// test if there are any equal values in the same row 3 apart with two 0's in the middle
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length - 3; j++) {
+				if (board[i][j] == board[i][j + 3] && board[i][j + 1] == 0 && board[i][j+2] == 0) {
+					testRight = true;
+				} 
+			}
+		}
+		//test if there is a 0 directly to the right of any element, if there is then moving to the right is possible
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length - 1; j++) {
+				if (board[i][j+1] == 0) {
+					testRight = true;
+				} 
+			}
+		}
+		return testRight;
+
+	}
+
+	// method to test if you can make a move to the left
+	public static boolean testLeft(int[][] board) {
 		
-		return Test;
+		
+		
+		
+		
+
+		return false;
 	}
-	
-	public static void main (String[] args) {
-		System.out.println(Test());
-		Test();
+
+	// method to test if you can make a move up
+	public static boolean testUp(int[][] board) {
+
+		return true;
 	}
-	
+
+	// method to test if you can make a move to the right
+	public static boolean testDown(int[][] board) {
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+	}
 
 }
+
