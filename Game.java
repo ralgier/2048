0da9c63f03b938540a7ffba234fe47fe6d1a320a
ralgier <36775917+ralgier@users.noreply.github.com>
2018-10-03 23:17:21 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,287 +1,263 @@
 import java.util.Random;
-
 import java.util.Scanner;
 
 public class Game {
-
+	// create a static int to increment the number of moves throughout
 	static int moveCounter = 0;
 
-
+	// main method
 	public static void main(String[] args) {
-		
+		// create the board and set the dimensions to 4x4
 		int[][] theBoard = new int[4][4];
-
 		Random newNum = new Random();
-
 		Scanner scan = new Scanner(System.in);
-
-		int numRows = 4;
-
-		int numColumns = 4;
-
 		int counter = 0;
 
-		String movement = new String();
-
-		String restart = new String();
-
-		String quit = new String();
-
+		// set the board to all 0's to begin with
 		for (int row = 0; row < theBoard.length; row++) {
-
 			for (int col = 0; col < theBoard[row].length; col++) {
-
 				theBoard[row][col] = 0;
-
 			}
-
 		}
-		//create new variables to create randoms for the row and column location
+		// create new variables to create randoms for the row and column location
 		int x = newNum.nextInt(100) + 1;
 		int y = newNum.nextInt(100) + 1;
 		int randRow = newNum.nextInt(4);
 		int randCol = newNum.nextInt(4);
 		int randRow2 = newNum.nextInt(4);
 		int randCol2 = newNum.nextInt(4);
-
+		// run while the random rows are not the same in case the random location is the
+		// same
 		while (theBoard[randRow] == theBoard[randRow2] && theBoard[randCol] == theBoard[randCol2]) {
-
 			randRow2 = newNum.nextInt(4);
-
 			randCol2 = newNum.nextInt(4);
-
 		}
-
+		// set the probability to 80% for x and 20% for 4 being randomly generated
 		if (x < 80) {
-
 			theBoard[randRow][randCol] = 2;
-
 		} else {
-
 			theBoard[randRow][randCol] = 4;
-
 		}
-
 		if (counter == 0) {
-
 			if (y < 80) {
-
 				theBoard[randRow2][randCol2] = 2;
-
 			} else {
-
 				theBoard[randRow2][randCol2] = 4;
-
 			}
-
 		}
 
 		for (int row = 0; row < theBoard.length; row++) {
-
 			for (int col = 0; col < theBoard[row].length; col++) {
-
-				System.out.print(theBoard[row][col] + " ");
-
+				System.out.print(theBoard[row][col] + "\t");
 			}
-
 			System.out.println(" ");
-
 		}
-
-		char typed;
-
+		char typed = 'e';
+		// as long as there is a vaild move to be made, enter the move choice loop
 		do {
-
+			// if (endGame()) {
 			System.out.println("w = up; a = left; s = down; d = right; q = quit; r = restart: ");
-
 			typed = scan.next().charAt(0);
-
 			if (typed == 'w') {
 
 				moveUp(theBoard);
 
-			}
-
-			else if (typed == 'a') {
+			} else if (typed == 'a') {
 
 				moveLeft(theBoard);
 
-			}
-
-			else if (typed == 's') {
+			} else if (typed == 's') {
 
 				moveDown(theBoard);
 
-			}
-
-			else if (typed == 'd') {
+			} else if (typed == 'd') {
 
 				moveRight(theBoard);
 
-			}
-
-			else if (typed == 'r') {
+			} else if (typed == 'r') {
 
 				System.out.print("Are you sure? Enter r again to restart or w to keep playing: ");
 				typed = scan.next().charAt(0);
 				if (typed == 'r') {
 
 					restart();
-				}
-			}
 
-			else if (typed == 'q') {
+				}
+			} else if (typed == 'q') {
 
 				System.out.print("Are you sure? Enter q again to quit or w to keep playing: ");
-
 				typed = scan.next().charAt(0);
-
 				if (typed == 'q') {
 
 					quit();
 
 				}
-
-			}
-
-			else {
-
+				// if the user did not type any of the letters that serve a function, tell them
+				// to enter another
+			} else {
 				System.out.println("That is not a valid input. Please enter w,a,s,d,r or q");
-
 			}
-			System.out.println("Number of moves made: "+moveCounter);
-		} while (typed != 'q'); // Deleted r because this still needs to run when program restarts.
 
+			// print out the number of moves made and the max value on the board after each
+			// move
+			System.out.println("Number of moves made: " + moveCounter);
+			printMax(theBoard);
+			counter++;
+			// }
+		} while (typed != 'q' && endGame());
 	}
 
-	public static void newRandomNumber(int[][] board) {
-
+	public static void newRandomNumber(int[][] theBoard) {
 		Random newNum = null;
-
 		int a = newNum.nextInt(100) + 1;
-
 		int b = newNum.nextInt(100) + 1;
-
 		int randRow = newNum.nextInt(4);
-
 		int randCol = newNum.nextInt(4);
-
 		// we need to put this in some sort of loop to check every value of the array
 
 		if (a < 80) {
-
 			// we need to test if the randomly selected location is empty or not, else we
 			// need to run again
-
-			if (board[randRow][randCol] == 0) {
-
-				board[randRow][randCol] = 2;
-
+			if (theBoard[randRow][randCol] == 0) {
+				theBoard[randRow][randCol] = 2;
 			}
-
 		} else {
 
-			if (board[randRow][randCol] == 0) {
-
-				board[randRow][randCol] = 4;
-
+			if (theBoard[randRow][randCol] == 0) {
+				theBoard[randRow][randCol] = 4;
 			}
-
 		}
-
 		// print out the new board again
-
-		for (int row = 0; row < board.length; row++) {
-
-			for (int col = 0; col < board[row].length; col++) {
-
-				System.out.print(board[row][col] + " ");
-
-			}
-
-			System.out.println(" ");
-
-		}
-
+		printBoard(theBoard);
 	}
 
-	public static void moveUp(int [][] theBoard) {
+	public static void moveUp(int[][] theBoard) {
+		theBoard[0][0] = 1;
+		theBoard[0][1] = 0;
+		theBoard[0][2] = 0;
+		theBoard[0][3] = 1;
+		theBoard[1][0] = 1;
+		theBoard[1][1] = 6;
+		theBoard[1][2] = 7;
+		theBoard[1][3] = 8;
+		theBoard[2][0] = 1;
+		theBoard[2][1] = 10;
+		theBoard[2][2] = 11;
+		theBoard[2][3] = 12;
+		theBoard[3][0] = 13;
+		theBoard[3][1] = 14;
+		theBoard[3][2] = 15;
+		theBoard[3][3] = 16;
+		
+		if(createBoard.testUp(theBoard)) {
+			System.out.println("test is true");
+		} else {
+			System.out.println("the test is false");
+		}
 
 		System.out.println("up");
 
-		// Need an if statement with a boolean here to check if move can happen
-
-		for (int i = theBoard.length - 1; i > 0; i--) {
-
-			for (int j = 0; j < theBoard.length; j++) {
-
-				theBoard[i - 1][j] = theBoard[j][theBoard[i - 1][j] + theBoard[i][j]];
-
-			}
-
-		}
-
+		printBoard(theBoard);
 		moveCounter++;
 
 	}
 
-	public static void moveLeft(int [][] theBoard) {
-
+	public static void moveLeft(int[][] theBoard) {
+		theBoard[0][0] = 0;
+		theBoard[0][1] = 0;
+		theBoard[0][2] = 0;
+		theBoard[0][3] = 0;
+		theBoard[1][0] = 1;
+		theBoard[1][1] = 6;
+		theBoard[1][2] = 7;
+		theBoard[1][3] = 8;
+		theBoard[2][0] = 9;
+		theBoard[2][1] = 10;
+		theBoard[2][2] = 11;
+		theBoard[2][3] = 12;
+		theBoard[3][0] = 13;
+		theBoard[3][1] = 14;
+		theBoard[3][2] = 15;
+		theBoard[3][3] = 16;
+		
+		if(createBoard.testLeft(theBoard)) {
+			System.out.println("test is true");
+		} else {
+			System.out.println("the test is false");
+		}
 		System.out.println("left");
 
-		// Need an if statement with a boolean here to check if move can happen
-
-		for (int i = 0; i > theBoard.length; i++) {
-
-			for (int j = theBoard.length - 1; j > 0; j--) {
-
-			}
-
-		}
+		printBoard(theBoard);
 
 		moveCounter++;
-
 	}
 
-	public static void moveDown(int [][] theBoard) {
-
+	public static void moveDown(int[][] theBoard) {
+		
+		theBoard[0][0] = 1;
+		theBoard[0][1] = 2;
+		theBoard[0][2] = 2;
+		theBoard[0][3] = 1;
+		theBoard[1][0] = 1;
+		theBoard[1][1] = 6;
+		theBoard[1][2] = 7;
+		theBoard[1][3] = 8;
+		theBoard[2][0] = 9;
+		theBoard[2][1] = 10;
+		theBoard[2][2] = 11;
+		theBoard[2][3] = 12;
+		theBoard[3][0] = 13;
+		theBoard[3][1] = 14;
+		theBoard[3][2] = 15;
+		theBoard[3][3] = 16;
+		
+		if(createBoard.testDown(theBoard)) {
+			System.out.println("test is true");
+		} else {
+			System.out.println("the test is false");
+		}
+		
 		System.out.println("down");
-
-		// Need an if statement with a boolean here to check if move can happen
-
-		for (int i = 0; i > theBoard.length - 1; i++) {
-
-			for (int j = 0; j < theBoard.length; j++) {
-
-			}
-
-		}
-
 		moveCounter++;
-
 	}
 
-	public static void moveRight(int [][] theBoard) {
-
-		System.out.println("right");
-
-		// Need an if statement with a boolean here to check if move can happen
-
-		for (int i = 0; i > theBoard.length; i++) {
-
-			for (int j = 0; j < theBoard.length - 1; j++) {
-
-			}
-
+	public static void moveRight(int[][] theBoard) {
+		theBoard[0][0] = 0;
+		theBoard[0][1] = 0;
+		theBoard[0][2] = 0;
+		theBoard[0][3] = 0;
+		theBoard[1][0] = 1;
+		theBoard[1][1] = 6;
+		theBoard[1][2] = 7;
+		theBoard[1][3] = 8;
+		theBoard[2][0] = 19;
+		theBoard[2][1] = 10;
+		theBoard[2][2] = 11;
+		theBoard[2][3] = 12;
+		theBoard[3][0] = 13;
+		theBoard[3][1] = 14;
+		theBoard[3][2] = 15;
+		theBoard[3][3] = 16;
+		
+		
+		if (createBoard.testRight(theBoard)) {
+			System.out.println("test right works");
+			System.out.println("true");
+			moveCounter++;
+		}else {
+			System.out.println("Test false");
+				//theBoard[0][1] = theBoard [0][0];
+				//theBoard[0][0] = 0;
 		}
 
-		moveCounter++;
-
+		printBoard(theBoard);
 	}
 
 	public static void restart() {
 
 		System.out.println("Restarting...");
+		System.out.println(" ");
+		System.out.println(" ");
 
 		String[] argument = new String[] { "1,2,3" };
 
@@ -290,8 +266,46 @@ public class Game {
 	}
 
 	public static void quit() {
-
+		// create a boolen method win/ no moves left
+		// call them both here and use if statments to print out the correct term
+		// at the end of all the methods set typed = q so the for loop exits
 		System.out.println("game over. Moves made: " + moveCounter);
+	}
+
+	// create method print 2d Array that takes in a 2d array as a parameter
+	public static void printBoard(int[][] theBoard) {
+		// create a nested for loop to go through all elements of the array
+		for (int i = 0; i < theBoard.length; i++) {
+			for (int j = 0; j < theBoard.length; j++) {
+				// print out each element with formatting
+				// the \t auto-formats the array when added
+				System.out.print(theBoard[i][j] + "\t");
+			}
+			// print out to go to the next line once you go through the whole row each time
+			System.out.println(" ");
+		}
+	}
+
+	// create method to print the current maximum of the array (needs to be called
+	// every time)
+	public static void printMax(int[][] board) {
+		int max = 2;
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+
+				if (board[row][col] > max) {
+					max = board[row][col];
+				}
+
+			}
+		}
+		System.out.println("The maximum value on the Board is: " + max);
+	}
+
+	// create boolean method to test if game is ended
+	public static boolean endGame() {
+		boolean endGame = true;
+		return endGame;
 
 	}
 
--- createBoard.java
+++ createBoard.java
@@ -1,11 +1,10 @@
-
 public class createBoard {
-	// method to test if the board is full, returns value of boolean
-	public static boolean boardFull(int[][] theBoard) {
 
+	// method to test if the board is full, returns value of boolean
+
+	public static boolean boardFull(int[][] theBoard) {
 		int rows = theBoard.length;
 		int columns = theBoard[1].length;
-
 		// create a counter
 		int counter = 0;
 		// make a boolean to set to true if the board is full
@@ -13,7 +12,6 @@ public class createBoard {
 		// create a nested for loop to go through every element of the 2d array
 		for (int row = 0; row < theBoard.length; row++) {
 			for (int col = 0; col < theBoard[row].length; col++) {
-
 				// if the specific location is not 0, there is a value there and that spot is
 				// full
 				if (theBoard[row][col] != 0) {
@@ -24,7 +22,6 @@ public class createBoard {
 				if (counter == (rows) * (columns)) {
 					boardFull = true;
 				}
-
 			}
 		}
 		return boardFull;
@@ -36,165 +33,152 @@ public class createBoard {
 		int max = 2;
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[row].length; col++) {
-
 				if (board[row][col] > max) {
 					max = board[row][max];
 				}
-
 			}
 		}
-		System.out.println(max);
 
+		System.out.println(max);
 	}
 
 	// method to test if you can make a move to the right
-	//
 	public static boolean testRight(int[][] board) {
 		boolean testRight = false;
 		// test if there is a location where there are two spaces in the same row next
 		// to each other that are equal
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length - 1; j++) {
-				if (board[i][j] == board[i][j + 1]) {
+				if (board[i][j] == board[i][j + 1] && board[i][j] != 0) {
 					testRight = true;
 				}
 			}
 		}
-		// test if there are any equal values in the same row 2 apart with a 0 in the middle
+		// test if there are any equal values in the same row 2 apart with a 0 in the
+		// middle
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length - 2; j++) {
-				if (board[i][j] == board[i][j + 2] && board[i][j + 1] == 0) {
+				if (board[i][j] == board[i][j + 2] && board[i][j + 1] == 0 && board[i][j] != 0) {
 					testRight = true;
-				} 
+				}
 			}
 		}
-		// test if there are any equal values in the same row 3 apart with two 0's in the middle
+		// test if there are any equal values in the same row 3 apart with two 0's in
+		// the middle
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length - 3; j++) {
-				if (board[i][j] == board[i][j + 3] && board[i][j + 1] == 0 && board[i][j+2] == 0) {
+				if (board[i][j] == board[i][j + 3] && board[i][j + 1] == 0 && board[i][j + 2] == 0 && board[i][j] != 0) {
 					testRight = true;
-				} 
+				}
 			}
 		}
-		//test if there is a 0 directly to the right of any element, if there is then moving to the right is possible
+		// test if there is a 0 directly to the right of any element, if there is then
+		// moving to the right is possible
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length - 1; j++) {
-				if (board[i][j+1] == 0) {
+				if (board[i][j + 1] == 0 && board[i][j] != 0) {
 					testRight = true;
-				} 
+				}
 			}
 		}
-		return testRight;	
+		return testRight;
 	}
-public static boolean testLeft(int[][] board) {
+
+	public static boolean testLeft(int[][] board) {
 		boolean testLeft = false;
-		// test if there is a location where there are one spaces in the same row to the left
+		// test if there is a location where there are one spaces in the same row to the
+		// lef
 		// to each other that are equal
 		for (int i = 0; i < board.length; i++) {
-			for (int j = board[i].length - 1; j > 0; j--) {
-				if (board[i][j] == board[i][j - 1]) {
+			for (int j = board.length - 1; j > 1; j--) {
+				if (board[i][j] == board[i][j - 1] && board[i][j] != 0) {
 					testLeft = true;
 				}
 			}
 		}
-		// test if there are any equal values in the same row 2 apart with a 0 in the middle
+		// test if there are any equal values in the same row 2 apart with a 0 in the
+		// middle
 		for (int i = 0; i < board.length; i++) {
-			for (int j = board[i].length - 2; j > 0 ; j--) {
-				if (board[i][j] == board[i][j - 2] && board[i][j - 1] == 0) {
+			for (int j = board[i].length - 2; j > 1; j--) {
+				if (board[i][j] == board[i][j - 2] && board[i][j - 1] == 0 && board[i][j] != 0) {
 					testLeft = true;
-				} 
+				}
 			}
 		}
-		// test if there are any equal values in the same row 3 apart with two 0's in the middle
+		// test if there are any equal values in the same row 3 apart with two 0's in
+		// the middle
 		for (int i = 0; i < board.length; i++) {
-			for (int j = board[i].length - 3; j > 0; j++) {
-				if (board[i][j] == board[i][j - 3] && board[i][j - 2] == 0 && board[i][j - 1] == 0) {
+			for (int j = board[i].length - 3; j > 1; j--) {
+				if (board[i][j] == board[i][j - 3] && board[i][j - 2] == 0 && board[i][j - 1] == 0 && board[i][j] != 0) {
 					testLeft = true;
-				} 
+				}
 			}
 		}
-		//test if there is a 0 directly to the left of any element, if there is then moving to the left is possible
+		// test if there is a 0 directly to the left of any element, if there is then
+		// moving to the left is possible
 		for (int i = 0; i < board.length; i++) {
-			for (int j = board[i].length - 1; j > 0; j--) {
-				if (board[i][j-1] == 0) {
+			for (int j = board[i].length - 1; j > 1; j--) {
+				if (board[i][j - 1] == 0 && board[i][j] != 0) {
 					testLeft = true;
-				} 
+				}
 			}
 		}
 		return testLeft;
 	}
 	
+	
 	public static boolean testUp(int[][] board) {
 		boolean testUp = false;
-		// test if there is a location where there are one spaces above 
+		// test if there is a location where there are one spaces above
 		// each other that are equal
-		for (int i = board.length - 1; i > 0 ; i--) {
-			for (int j = 0; j > board.length; j++) {
-				if (board[i][j] == board[i-1][j]) {
+		for (int i = board.length - 1; i > 0; i--) {
+			for (int j = 0; j > board.length-1; j++) {
+				if (board[i][j] == board[i - 1][j]) {
 					testUp = true;
 				}
-			}
-		}
-		// test if there are any equal values in the same column 2 apart with a 0 in the middle
-		for (int i = board.length - 2; i > 0; i--) {
-			for (int j = 0; j > board.length; j++) {
-				if (board[i][j] == board[i - 2][j] && board[i - 1][j] == 0) {
-					testUp = true;
-				} 
-			}
-		}
-		// test if there are any equal values in the same column 3 apart with two 0's in the middle
-		for (int i = board.length - 3; i < 0; i--) {
-			for (int j = 0; j > board.length; j++) {
-				if (board[i][j] == board[i-3][j] && board[i-2][j] == 0 && board[i-1][j] == 0) {
-					testUp = true;
-				} 
-			}
-		}
-		//test if there is a 0 directly above any element, if there is then moving to the left is possible
-		for (int i = board.length; i > 0; i--) {
-			for (int j = 0; j > board.length; j++) {
-				if (board[i-1][j] == 0) {
-					testUp = true;
-				} 
 			}
 		}
 		return testUp;
 	}
 	
+	
+	
 	public static boolean testDown(int[][] board) {
 		boolean testDown = false;
-		// test if there is a location where there are one spaces above 
+		// test if there is a location where there are one spaces above
 		// each other that are equal
-		for (int i = 0; i > board.length - 1 ; i++) {
+		for (int i = 0; i > board.length - 1; i++) {
 			for (int j = 0; j > board.length; j++) {
-				if (board[i][j] == board[i+1][j]) {
+				if (board[i][j] == board[i + 1][j]) {
 					testDown = true;
 				}
 			}
 		}
-		// test if there are any equal values in the same column 2 apart with a 0 in the middle
+		// test if there are any equal values in the same column 2 apart with a 0 in the
+		// middle
 		for (int i = 0; i > board.length - 2; i++) {
 			for (int j = 0; j > board.length; j++) {
 				if (board[i][j] == board[i + 2][j] && board[i + 1][j] == 0) {
 					testDown = true;
-				} 
+				}
 			}
 		}
-		// test if there are any equal values in the same column 3 apart with two 0's in the middle
+		// test if there are any equal values in the same column 3 apart with two 0's in
+		// the middle
 		for (int i = 0; i < board.length - 3; i++) {
 			for (int j = 0; j > board.length; j++) {
-				if (board[i][j] == board[i+3][j] && board[i+2][j] == 0 && board[i+1][j] == 0) {
+				if (board[i][j] == board[i + 3][j] && board[i + 2][j] == 0 && board[i + 1][j] == 0) {
 					testDown = true;
-				} 
+				}
 			}
 		}
-		//test if there is a 0 directly above any element, if there is then moving to the left is possible
+		// test if there is a 0 directly above any element, if there is then moving to
+		// the left is possible
 		for (int i = 0; i > board.length - 1; i++) {
 			for (int j = 0; j > board.length; j++) {
-				if (board[i+1][j] == 0) {
+				if (board[i + 1][j] == 0) {
 					testDown = true;
-				} 
+				}
 			}
 		}
 		return testDown;
@@ -205,3 +189,10 @@ public static boolean testLeft(int[][] board) {
 	}
 
 }
+
+// new columns than what he started with
+//
+
+// need to use array list to get the values where there are o and put that in
+// the list and only randomize from that list
+//
